31b0255cefe72c3a551656fcdd5fb1090c248d41
Carlos Silva <silvacj90@gmail.com>
2023-10-17 15:20:30 -0400

    add animation and smooth transitions with JS

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 a personal portfolio website
+
+Website Must Include
+-Index page
+-index page will showcase
+-Portfolio page
+-About page
+-Contact page
--- app.js
+++ app.js
@@ -2,18 +2,22 @@ const enterBtn = document.querySelector(".btn-begin");
 const header = document.querySelector("header");
 const bannerImg = document.querySelector("#logo-art");
 const greeting = document.querySelector(".greeting-container");
+const quoteblock = document.querySelector("#quoteblock");
+const main = document.querySelector("main");
+const overflowController = document.querySelector("html, body, main");
 
 const dropCurtain = (_callback) => {
   header.style.height = `${92}%`;
   setTimeout(() => {
     _callback();
     vanishHeaderContent();
-  }, 2100);
+  }, 1700);
 };
 
 const liftCurtain = () => {
   dropCurtain(function () {
     header.style.height = `${10}%`;
+    contentAppear();
   });
 };
 
@@ -22,6 +26,11 @@ const vanishHeaderContent = () => {
   bannerImg.style.visibility = "hidden";
   greeting.style.opacity = 0;
   greeting.style.visibility = "hidden";
+};
+
+const contentAppear = () => {
+  main.style.opacity = 1;
+  overflowController.style.overflowY = "scroll";
 };
 
 enterBtn.addEventListener("click", function () {
