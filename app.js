6c21e8cae23a7ca3d89f24e18db002858386a9e8
Carlos Silva <silvacj90@gmail.com>
2023-10-18 11:48:57 -0400

    fix nav animation and functionality

--- app.js
+++ app.js
@@ -5,7 +5,19 @@ const greeting = document.querySelector(".greeting-container");
 const quoteblock = document.querySelector("#quoteblock");
 const main = document.querySelector("main");
 const overflowController = document.querySelector("html, body, main");
+const botNav = document.querySelector(".bot-nav");
+const navBurger = document.querySelector(".burger-nav");
+const navLinks = document.querySelector(".nav-links");
+
 header.style.minHeight = 450;
+
+const vanishHeaderContent = () => {
+  bannerImg.style.opacity = 0;
+  bannerImg.style.visibility = "hidden";
+  greeting.style.opacity = 0;
+  greeting.style.visibility = "hidden";
+  botNav.style.display = "none";
+};
 
 const dropCurtain = (_callback) => {
   header.style.height = `${92}%`;
@@ -15,27 +27,29 @@ const dropCurtain = (_callback) => {
   }, 1400);
 };
 
-const liftCurtain = () => {
-  dropCurtain(function () {
-    header.style.height = `${10}%`;
-    header.style.minHeight = 100;
-    contentAppear();
-  });
-};
-
-const vanishHeaderContent = () => {
-  bannerImg.style.opacity = 0;
-  bannerImg.style.visibility = "hidden";
-  greeting.style.opacity = 0;
-  greeting.style.visibility = "hidden";
-};
-
 const contentAppear = () => {
   main.style.opacity = 1;
   overflowController.style.overflowY = "scroll";
 };
 
-enterBtn.addEventListener("click", function () {
+const liftCurtain = () => {
+  dropCurtain(function () {
+    header.style.height = `${10}%`;
+    header.style.minHeight = null;
+    contentAppear();
+  });
+};
+
+const enterButton = () => {
   enterBtn.style.display = "none";
   liftCurtain();
+};
+
+const navToggle = () => {
+  header.classList.toggle("header-effect");
+  navLinks.classList.toggle("nav-effect");
+};
+
+navBurger.addEventListener("click", function () {
+  navToggle();
 });
